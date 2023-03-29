d78537271a55fb7d5da49a14d122c41713ad83a4
Ja'far Malik Ibrahim <92065895+JafarMalikIbrahim@users.noreply.github.com>
2023-03-29 09:55:10 +0700

    Konversi_suhu2.0

--- lib/widget/view.dart
+++ lib/widget/view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:konversi_suhu/widget/convert.dart';
 import 'package:konversi_suhu/widget/input.dart';
+import 'package:konversi_suhu/widget/result.dart';
 import '../main.dart';
 import 'mytext.dart';
 
@@ -15,6 +16,11 @@ class _ViewState extends State<View> {
   double _inputUser = 0;
   double _kelvin = 0;
   double _reamur = 0;
+  String _newValue = "Kelvin";
+  double _result= 0;
+
+  List<String> listViewItem = <String>[];
+  var listItem = ["Kelvin", "Reamur"];
 
   final etInput = TextEditingController();
 
@@ -29,9 +35,12 @@ class _ViewState extends State<View> {
   void konversiSuhu() {
     setState(() {
       _inputUser = double.parse(etInput.text);
-      _reamur = (_inputUser - 273.15) * 0.8;
-      _kelvin = 273.15 + _inputUser;
-    });
+        if (_newValue == "Kelvin")
+        _result = _inputUser + 273;
+        else
+        _result = (4 / 5) * _inputUser;
+        });
+        listViewItem.add(_result.toString());
   }
 
   @override
@@ -41,22 +50,55 @@ class _ViewState extends State<View> {
       child: Column(
         children: [
           Input(etInput: etInput),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                MyText(
-                    text1: 'Suhu dalam Kelvin',
-                    text2: _kelvin.toStringAsFixed(2)),
-                MyText(
-                    text1: 'Suhu dalam Reamor',
-                    text2: _reamur.toStringAsFixed(2)),
-              ],
+          DropdownButton<String>(
+              items: listItem.map(myDropDownMenuItem).toList(),
+              value: _newValue,
+              onChanged: onChangeDropDown),
+              Result(result: _result),
+          // Expanded(
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //     children: <Widget>[
+          //       MyText(
+          //           text1: 'Suhu dalam Kelvin',
+          //           text2: _kelvin.toStringAsFixed(2)),
+          //       MyText(
+          //           text1: 'Suhu dalam Reamor',
+          //           text2: _reamur.toStringAsFixed(2)),
+          //     ],
+          //   ),
+          // ),
+          Convert(konvertHandler: konversiSuhu),
+          Container(
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
+            child: const Text("Riwayat Konversi",
+            style: TextStyle(fontSize: 20),
+            ),
+            ),
+            Expanded(
+            child: ListView(
+              children: listViewItem.map((String value) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Text(
+                  value,
+                  style: const TextStyle(fontSize: 15),
+                ));
+              }).toList()
             ),
           ),
-          Convert(konvertHandler: konversiSuhu),
         ],
       ),
     );
+  }
+  void onChangeDropDown(String? changeValue) {
+    setState(() {
+      _newValue = changeValue!;
+      konversiSuhu();
+    });
+  }
+
+  DropdownMenuItem<String> myDropDownMenuItem(String value) {
+    return DropdownMenuItem<String>(value: value, child: Text(value));
   }
 }
